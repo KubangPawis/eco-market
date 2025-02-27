9f453626ebe904e0295ad847db52a4d9b8fa386a
KubangPawis <xanderfernandezperegrin@gmail.com>
2025-02-27 10:32:56 +0800

    feat: create sign up page

---
+++ lib/pages/sign_up_page.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('EcoMarket',
+            style: GoogleFonts.poppins(
+              textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            )),
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(15),
+          topRight: Radius.circular(15),
+        ),
+        child: BottomAppBar(
+          color: Color.fromARGB(255, 16, 47, 21),
+        ),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // LOGIN SPLASH IMAGE
+            Image.asset('assets/images/sign_up_splash.png',
+                width: 600, height: 500),
+
+            // SPACER
+            SizedBox(
+              width: 50,
+            ),
+
+            // LOGIN FORM
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Sign Up',
+                    style: GoogleFonts.poppins(
+                      textStyle:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+
+                  // EMAIL TEXT FIELD
+                  SizedBox(height: 20),
+                  Text(
+                    'EMAIL',
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 800,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        //labelText: 'Enter your name',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+
+                  // PASSWORD TEXT FIELD
+                  SizedBox(height: 10),
+                  Text(
+                    'PASSWORD',
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 800,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                      obscureText: true,
+                    ),
+                  ),
+
+                  // CONFIRM PASSWORD TEXT FIELD
+                  SizedBox(height: 10),
+                  Text(
+                    'CONFIRM PASSWORD',
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 800,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                      obscureText: true,
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+
+                  SizedBox(
+                    width: 700,
+                    height: 200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        //LOG IN BUTTON
+                        SizedBox(
+                            width: double.infinity,
+                            child: TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                backgroundColor: Colors.yellow,
+                                foregroundColor: Colors.black,
+                                padding: EdgeInsets.all(15),
+                              ),
+                              child: Text(
+                                'CREATE ACCOUNT',
+                                style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                            )),
+
+                        //SIGN UP BUTTON
+                        SizedBox(height: 10),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: double.infinity,
+                              child: OutlinedButton(
+                                onPressed: () {},
+                                style: OutlinedButton.styleFrom(
+                                  side:
+                                      BorderSide(color: Colors.black, width: 1),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  foregroundColor: Colors.black,
+                                  padding: EdgeInsets.all(15),
+                                ),
+                                child: Text(
+                                  'BACK TO LOGIN',
+                                  style: GoogleFonts.poppins(
+                                    textStyle: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
