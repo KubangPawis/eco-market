086b99546274628677eb767dd2dcd2f4876f8f1b
Rainzle Estuesta <rjestuesta@gmail.com>
2025-03-12 09:13:28 +0800

    deleted: firebase config (used an .env file instead).

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_config.dart'; // Import your config file
 import 'package:eco_market/pages/login_page.dart';
 import 'package:eco_market/pages/sign_up_page.dart';
 import 'package:eco_market/pages/profile_page.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize Firebase using the config from a separate file
-  await Firebase.initializeApp(options: firebaseConfig);
+  // Load the .env file
+  await dotenv.load(fileName: ".env");
+  print("API_KEY: ${dotenv.env['API_KEY']}");
+
+  // Initialize Firebase using values from the .env file
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: dotenv.env['API_KEY']!,
+      authDomain: dotenv.env['AUTH_DOMAIN']!,
+      projectId: dotenv.env['PROJECT_ID']!,
+      storageBucket: dotenv.env['STORAGE_BUCKET']!,
+      messagingSenderId: dotenv.env['MESSAGING_SENDER_ID']!,
+      appId: dotenv.env['APP_ID']!,
+      measurementId: dotenv.env['MEASUREMENT_ID']!,
+    ),
+  );
 
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,7 +40,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/login': (context) => LoginPage(),
         '/signup': (context) => SignUpPage(),
-        '/profile': (context) => ProfilePage()
+        '/profile': (context) => ProfilePage(),
       },
     );
   }
