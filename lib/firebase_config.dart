409fd41116ad327af66d243094fb4c5082a8b8bb
Rainzle Estuesta <rjestuesta@gmail.com>
2025-03-12 10:44:38 +0800

    added multiple files: added uploading of products for the backend

---
+++ lib/firebase_config.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+
+const firebaseConfig = FirebaseOptions(
+  apiKey: "AIzaSyB8QHrWMHJIrjeb3eokI3IGSG8uqKaYFh8",
+  authDomain: "ecomarket-60acd.firebaseapp.com",
+  projectId: "ecomarket-60acd",
+  storageBucket: "ecomarket-60acd.firebasestorage.app",
+  messagingSenderId: "909870506837",
+  appId: "1:909870506837:web:a2be61c3fd1268387585dc",
+  measurementId: "G-0TK03F8FLK",
+);
